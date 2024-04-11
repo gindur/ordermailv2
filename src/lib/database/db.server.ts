b2759f3083bfdfa740db193f1b46a854f3a948f2
David Pettersson <dawei.98@hotmail.com>
2024-04-11 15:23:46 +0200

    commit

--- src/lib/database/db.server.ts
+++ src/lib/database/db.server.ts
@@ -1,9 +1,10 @@
 // db/db.server.ts
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { DATABASE_URL } from '$env/static/private';
+import env from '$env/dynamic/private';
 import { dev } from '$app/environment';
 
+
 // const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: 'require' });
-const client =  postgres(DATABASE_URL) 
+const client =  postgres(env.env.PRIVATE_DATABASE_URL) 
 export const db = drizzle(client, {});
